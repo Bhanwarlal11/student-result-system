fbf5f4b9a75e766b4615193e22f5d259a41b4131
bhanwarlal11 <bhawarpatel2016@gmail.com>
2025-05-14 19:28:15 +0530

    env added

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://student-result-system.onrender.com";
+
 
 /* ----------- Subjects API ----------- */
 
